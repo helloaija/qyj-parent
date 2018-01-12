9eb46cb5a3a25715caeda00c6c9dac895b62e948
shitongle <shitongle163@126.com>
2018-01-12 11:18:21 +0800

    添加默认首页

--- qyj-back/src/main/java/com/qyj/back/common/util/Utils.java
+++ qyj-back/src/main/java/com/qyj/back/common/util/Utils.java
@@ -1,10 +1,6 @@
 package com.qyj.back.common.util;
 
-import java.io.File;
 import java.io.Writer;
-import java.text.SimpleDateFormat;
-import java.util.Collection;
-import java.util.Date;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,21 +8,13 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.qyj.common.utils.CommonUtils;
 
-public class Utils {
-
-	/**
-	 * 判断集合是否为空
-	 * @param con
-	 * @return
-	 */
-	@SuppressWarnings("rawtypes")
-	public static boolean isEmptyCollection(Collection con) {
-		if (con == null || con.isEmpty()) {
-			return true;
-		}
-		return false;
-	}
+/**
+ * 工具类
+ * @author CTF_stone
+ */
+public class Utils extends CommonUtils {
 
 	/**
 	 * resonse返回字符串
@@ -78,70 +66,5 @@ public class Utils {
 		String path = request.getScheme() + "://" + request.getServerName() + serverPort + request.getContextPath();
 		return path;
 	}
-	
-	/**
-	 * 获取项目部署绝对路径（evan.webapp在web.xml中有配置）
-	 * @return
-	 */
-	public static String getWebAppPath() {
-		return System.getProperty("evan.webapp");
-	}
-	
-	/**
-	 * 获取tomcat根目录
-	 * @return
-	 */
-	public static String getTomcatPath() {
-		return System.getProperty("catalina.home");
-	}
-	
-	/**
-	 * 获取上传文件目录（保存在tomcat根目录中，获取文件需要配置虚拟目录）
-	 * @return
-	 */
-	public static String getUploadFilePath() {
-		File uploadFileDir = new File(getTomcatPath() + File.separator + "uploadFile");
-		if (!uploadFileDir.exists()) {
-			uploadFileDir.mkdirs();
-		}
-		return uploadFileDir.getAbsolutePath();
-	}
-	
-	public static void main(String[] args) {
-		System.out.println();
-	}
-	
-	/**
-	 * 获取随机数
-	 * @param min 最小数
-	 * @param max 最大数
-	 * @return int
-	 */
-	public static int getRandom(int min, int max) {
-		return (int) (Math.random() * (max - min) + min);
-	}
 
-	/**
-	 * 获取随机数从1开始,格式10000000-99999999
-	 * @param number 随机数长度
-	 * @return
-	 */
-	public static int getRandom(int number) {
-		int max = 9;
-		int min = 1;
-		for (int i = 1; i < number; i++) {
-			min = min * 10;
-			max = max * 10 + 9;
-		}
-		return getRandom(min, max);
-	}
-
-	/**
-	 * 12位时间加上number位数
-	 * @param number
-	 * @return Long
-	 */
-	public static Long getUid(int number) {
-		return Long.parseLong(new SimpleDateFormat("yyMMddHHmmss").format(new Date()) + getRandom(number));
-	}
 }
--- qyj-web/src/main/webapp/weChat/js/app.js
+++ qyj-web/src/main/webapp/weChat/js/app.js
@@ -6,128 +6,129 @@ define(["angular", "angular-ui-router", "oclazyload", "angular-sanitize", "angul
 	qyjApp.uploadFileHeader = "http://192.168.30.22:8082/qyj-back/uploadFile/";
 	qyjApp.uploadHeader = "http://192.168.30.22:8082/qyj-back/upload/";
 	
-	qyjApp.config(["$stateProvider", "$httpProvider",
-        function($stateProvider, $httpProvider) {
+	qyjApp.config(["$stateProvider", "$httpProvider", "$urlRouterProvider",
+        function($stateProvider, $httpProvider, $urlRouterProvider) {
+			$urlRouterProvider.when('', '/home/index');
 	   		// 定义路由
 	   		$stateProvider.state("home", {
 	   			url : "/home",
-	   			templateUrl : "../page/home.html"
+	   			templateUrl : "weChat/page/home.html"
 	   		}).state("home.index", {
 	   			url : "/index",
 	   			// 产品列表页面
-	   			templateUrl : "../page/home/homeContent.html",
+	   			templateUrl : "weChat/page/home/homeContent.html",
 	   			controller : "homeCtrl",
 	   			resolve : {
 	   				homeCtrl : ['$ocLazyLoad', function($ocLazyLoad) {
-	   					return $ocLazyLoad.load(['../js/controller/homeController.js']);
+	   					return $ocLazyLoad.load(['weChat/js/controller/homeController.js']);
 	   				}]
 	   	        }
 	   		}).state("home.productList", {
 	   			url : "/productList",
 	   			// 产品列表页面
-	   			templateUrl : "../page/product/productList.html",
+	   			templateUrl : "weChat/page/product/productList.html",
 	   			controller : "productCtrl",
 	   			resolve : {
 	   				productCtrl : ['$ocLazyLoad', function($ocLazyLoad) {
-	   					return $ocLazyLoad.load(["ng-infinite-scroll", '../js/controller/productController.js',
-	   					                      '../js/service/productService.js']);
+	   					return $ocLazyLoad.load(["ng-infinite-scroll", 'weChat/js/controller/productController.js',
+	   					                      'weChat/js/service/productService.js']);
 	   				}]
 	   	        }
 	   		}).state("home.newsList", {
 	   			url : "/newsList", 
 	   			// 新闻公告列表页面
-	   			templateUrl : "../page/news/newsList.html",
+	   			templateUrl : "weChat/page/news/newsList.html",
 	   			controller : "newsCtrl",
 	   			resolve : {
 	   				newsCtrl : ['$ocLazyLoad', function($ocLazyLoad) {
-	   					return $ocLazyLoad.load(["ng-infinite-scroll", '../js/controller/newsController.js',
-	   					                      '../js/service/newsService.js']);
+	   					return $ocLazyLoad.load(["ng-infinite-scroll", 'weChat/js/controller/newsController.js',
+	   					                      'weChat/js/service/newsService.js']);
 	   				}]
 	   			}
 	   		}).state("home.account", {
 	   			url : "/account", 
 	   			// 新闻公告列表页面
-	   			templateUrl : "../page/account/accountIndex.html",
+	   			templateUrl : "weChat/page/account/accountIndex.html",
 	   			controller : "accountController",
 	   			resolve : {
 	   				accountController : ['$ocLazyLoad', function($ocLazyLoad) {
-	   					return $ocLazyLoad.load(['../js/controller/accountController.js',
-	   					                         "../js/service/accountService.js"]);
+	   					return $ocLazyLoad.load(['weChat/js/controller/accountController.js',
+	   					                         "weChat/js/service/accountService.js"]);
 	   				}]
 	   			}
 	   		}).state("newsContent", {
 	   			url : "/newsContent?newsInfoId",
 	   			// 新闻公告内容
-	   			templateUrl : "../page/news/newsContent.html",
+	   			templateUrl : "weChat/page/news/newsContent.html",
 	   			controller : "newsContentCtrl",
 	   			resolve : {
 	   				newsContentCtrl : ['$ocLazyLoad', function($ocLazyLoad) {
-	   					return $ocLazyLoad.load(['../js/controller/newsController.js',
-	   					                      '../js/service/newsService.js']);
+	   					return $ocLazyLoad.load(['weChat/js/controller/newsController.js',
+	   					                      'weChat/js/service/newsService.js']);
 	   				}]
 	   			}
 	   		}).state("productContent", {
 	   			url : "/productContent?productId",
 	   			// 产品内容
-	   			templateUrl : "../page/product/productContent.html",
+	   			templateUrl : "weChat/page/product/productContent.html",
 	   			controller : "productContentCtrl",
 	   			resolve : {
 	   				productContentCtrl : ['$ocLazyLoad', function($ocLazyLoad) {
 	   					return $ocLazyLoad.load(["ui-bootstrap",
-	   											'../js/controller/productContentController.js',
-	   											'../js/service/productContentService.js']);
+	   											'weChat/js/controller/productContentController.js',
+	   											'weChat/js/service/productContentService.js']);
 	   				}]
 	   			}
 	   		}).state("login", {
 	   			url : "/login",
 	   			// 登录页
-	   			templateUrl : "../page/login/login.html",
+	   			templateUrl : "weChat/page/login/login.html",
 	   			controller : "loginCtrl",
 	   			resolve : {
 	   				loginCtrl : ['$ocLazyLoad', function($ocLazyLoad) {
-	   					return $ocLazyLoad.load(["../js/filter/commonFilter.js",
-	   					                         '../js/controller/loginController.js',
-	   					                         "../js/service/loginService.js"
+	   					return $ocLazyLoad.load(["weChat/js/filter/commonFilter.js",
+	   					                         'weChat/js/controller/loginController.js',
+	   					                         "weChat/js/service/loginService.js"
 	   					                         ]);
 	   				}]
 	   	        }
 	   		}).state("address", {
 	   			url : "/address?isSelect",
 	   			// 登录地址列表
-	   			templateUrl : "../page/account/addressList.html",
+	   			templateUrl : "weChat/page/account/addressList.html",
 	   			controller : "addressCtrl",
 	   			resolve : {
 	   				addressCtrl : ['$ocLazyLoad', function($ocLazyLoad) {
-	   					return $ocLazyLoad.load(["../js/service/addressService.js", '../js/controller/addressController.js']);
+	   					return $ocLazyLoad.load(["weChat/js/service/addressService.js", 'weChat/js/controller/addressController.js']);
 	   				}]
 	   			}
 	   		}).state("addressEdit", {
 	   			url : "/addressEdit?addressId",
 	   			// 地址编辑
-	   			templateUrl : "../page/account/addressEdit.html",
+	   			templateUrl : "weChat/page/account/addressEdit.html",
 	   			controller : "addressEditCtrl",
 	   			resolve : {
 	   				addressEditCtrl : ['$ocLazyLoad', function($ocLazyLoad) {
-	   					return $ocLazyLoad.load(["ui-bootstrap", '../js/directive/directives.js', "../js/service/commonServices.js",
-	   					                         "../js/service/addressService.js", '../js/controller/addressController.js']);
+	   					return $ocLazyLoad.load(["ui-bootstrap", 'weChat/js/directive/directives.js', "weChat/js/service/commonServices.js",
+	   					                         "weChat/js/service/addressService.js", 'weChat/js/controller/addressController.js']);
 	   				}]
 	   	        }
 	   		}).state("shoppingTrolley", {
 	   			url : "/shoppingTrolley",
 	   			// 购物车
-	   			templateUrl : "../page/account/shoppingTrolley.html"
+	   			templateUrl : "weChat/page/account/shoppingTrolley.html"
 	   		}).state("orderList", {
 	   			url : "/orderList",
 	   			// 订单列表
-	   			templateUrl : "../page/account/orderList.html"
+	   			templateUrl : "weChat/page/account/orderList.html"
 	   		}).state("confirmOrder", {
 	   			url : "/confirmOrder?productId",
 	   			// 确认订单页面
-	   			templateUrl : "../page/product/confirmOrder.html",
+	   			templateUrl : "weChat/page/product/confirmOrder.html",
 	   			controller : "confirmOrderCtrl",
 	   			resolve : {
 	   				addressEditCtrl : ['$ocLazyLoad', function($ocLazyLoad) {
-	   					return $ocLazyLoad.load(["../js/service/confirmOrderService.js", '../js/controller/confirmOrderController.js']);
+	   					return $ocLazyLoad.load(["weChat/js/service/confirmOrderService.js", 'weChat/js/controller/confirmOrderController.js']);
 	   				}]
 	   	        }
 	   		});
@@ -166,20 +167,20 @@ define(["angular", "angular-ui-router", "oclazyload", "angular-sanitize", "angul
 	       modules: [{
 	    	   events: true,
 	    	   name : "angular-sanitize",
-	    	   files : ["../js/base/angular-sanitize.min.js"]
+	    	   files : ["weChat/js/base/angular-sanitize.min.js"]
 	       }, {
 	    	   events: true,
 	    	   name : "angular-animate",
-	    	   files : ["../js/base/angular-animate-1.6.7.min.js"]
+	    	   files : ["weChat/js/base/angular-animate-1.6.7.min.js"]
 	       }, {
 	    	   events: true,
 	    	   name : "ui-bootstrap",
-	    	   files : ["../js/base/ui-bootstrap.min.js",
-	    	            "../js/base/ui-bootstrap-tpls-2.5.0.min.js"]
+	    	   files : ["weChat/js/base/ui-bootstrap.min.js",
+	    	            "weChat/js/base/ui-bootstrap-tpls-2.5.0.min.js"]
 	       }, {
 	    	   // 滚动加载
 	    	   name : "ng-infinite-scroll",
-	    	   files : ["../js/base/ng-infinite-scroll-1.0.0.min.js"]
+	    	   files : ["weChat/js/base/ng-infinite-scroll-1.0.0.min.js"]
 	       }],
 	       debug: true
 	   })
